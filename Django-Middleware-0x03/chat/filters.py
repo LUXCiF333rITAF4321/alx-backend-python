0053cd38c6177d41e5d232e7fa18b2cf057b2e2f
saad333 <saadouddah1@gmail.com>
2025-09-26 23:47:08 +0100

    Create filters.py

---
+++ Django-Middleware-0x03/chat/filters.py
@@ -0,0 +1,12 @@
+import django_filters
+from .models import Message
+
+
+class MessageFilter(django_filters.FilterSet):
+    sent_after = django_filters.DateTimeFilter(field_name="sent_at", lookup_expr="gte")
+    sent_before = django_filters.DateTimeFilter(field_name="sent_at", lookup_expr="lte")
+    sender = django_filters.NumberFilter(field_name="sender__user_id")
+
+    class Meta:
+        model = Message
+        fields = ["conversation", "sender", "sent_after", "sent_before"]
